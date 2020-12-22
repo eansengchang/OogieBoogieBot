269b44f10ce8587f77c33a32cc8d91c509f68469
eansengchang <76442284+eansengchang@users.noreply.github.com>
2020-12-22 00:10:30 +0000

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -134,7 +134,7 @@ client.on('message', (message) => {
 
     //random stuff
     if (message.channel.id === '715917628727885874') {
-        if (Math.random()< 0.14) {
+        if (Math.random()< 0.2) {
             message.channel.send('pain.');
         }
     }
