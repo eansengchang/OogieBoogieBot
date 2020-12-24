4e1645d456e3a17ae2d8037d518e7effd5b8b758
eansengchang <76442284+eansengchang@users.noreply.github.com>
2020-12-24 01:47:41 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     execute(message, args) {
-        let botping = Math.round(client.ws.ping);
+        let botping = Math.round(message.client.ws.ping);
 
         message.channel.send(`Pong! ${botping}ms`);
     },
