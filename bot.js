ae66daa490b612ab886b204475592c249eae9ca2
eansengchang <76442284+eansengchang@users.noreply.github.com>
2020-12-26 13:57:42 +0000

    removed whitespace before config

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const config = require('./ config.json');
+const config = require('./config.json');
 const fs = require('fs');
 
 const Discord = require('discord.js');
--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../ config.json');
+const config = require('../config.json');
 PREFIX = config.prefix;
 
 module.exports = {
