19beba3177ab36b1c3eec5bc450de6ab4628c954
eansengchang <76442284+eansengchang@users.noreply.github.com>
2020-12-26 21:21:34 +0000

    Update length.js

--- commands/length.js
+++ commands/length.js
@@ -4,6 +4,6 @@ module.exports = {
     
     execute(message, args) {
         const user = message.mentions.users.first() || message.author || message.member.user;
-        message.channel.send(`<@${user.id}>'s dick is ${member.id.substring(1, 2)} inches long`);
+        message.channel.send(`<@${user.id}>'s dick is ${user.id.substring(1, 2)} inches long`);
     },
 };
