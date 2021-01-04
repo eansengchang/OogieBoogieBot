2880704d93c97d139aa73aac37db66cb91b9e3bb
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-04 22:28:39 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,15 +16,5 @@ loadCommands(client);
 const loadEvents = require('@root/events/load-events');
 loadEvents(client);
 
-/*fs.readdir('./events/', (err, files) => {
-    if (err) return console.error;
-    files.forEach(file => {
-        if (!file.endsWith('.js')) return;
-        const evt = require(`./events/${file}`);
-        let evtName = file.split('.')[0];
-        client.on(evtName, evt.bind(null, client));
-    });
-});*/
-
 client.login(process.env.BOTTOKEN);
 client.mongoose.init();
