e698f9e50253c48992837d0ff1174c7a3aa027f9
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-04 23:17:14 +0000

    Update info.js

--- commands/commands/info commands/info.js
+++ commands/commands/info commands/info.js
@@ -11,20 +11,35 @@ module.exports = {
             channels += guild.channels.cache.size;
             serverMembers += guild.members.cache.size;
         });
-        
+
         let embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`Information on Ching Chong Bot`)
             .setThumbnail(message.client.user.displayAvatarURL())
             .addFields(
-                { name: 'Created by:', value: `ESC#3777` , inline: false },
-                { name: 'Created on:', value: `${message.client.user.createdAt.toDateString()}` , inline: false },
-                { name: 'Servers:', value: `${message.client.guilds.cache.size}` , inline: false },
-                { name: 'Channels:', value: `${channels}` , inline: false },
-                { name: 'Server members:', value: `${serverMembers}` , inline: false },
-                { name: 'Commands:', value: `${message.client.commands.size}` , inline: false },
-                { name: 'Uptime:', value: `Online since \`${message.client.readyAt.toDateString()}\`` , inline: false },
+                { name: 'Created by:', value: `ESC#3777`, inline: false },
+                { name: 'Created on:', value: `${message.client.user.createdAt.toDateString()}`, inline: false },
+                { name: 'Servers:', value: `${message.client.guilds.cache.size}`, inline: false },
+                { name: 'Channels:', value: `${channels}`, inline: false },
+                { name: 'Server members:', value: `${serverMembers}`, inline: false },
+                { name: 'Commands:', value: `${message.client.commands.size}`, inline: false },
             )
+        let uptime = message.client.uptime;
+        let days = Math.floor(uptime / 1000 / 60 / 60 / 24)
+        uptime -= days * 1000 * 60 * 60 * 24;
+
+        let hours = Math.floor(uptime / 1000 / 60 / 60)
+        uptime -= hours * 1000 * 60 * 60;
+
+        let minutes = Math.floor(uptime / 1000 / 60)
+        uptime -= minutes * 1000 * 60;
+
+        let seconds = Math.floor(uptime / 1000)
+        uptime -= seconds * 1000;
+
+        embed.addFields(
+            { name: 'Uptime:', value: `Online for \`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds\``, inline: false },
+        )
         message.channel.send(embed);
     },
 };
