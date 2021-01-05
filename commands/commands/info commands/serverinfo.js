ce41fa4f1486c3c2b3580dbb8a5bafe01f0defa5
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-05 20:40:47 +0000

    Update serverinfo.js

--- commands/commands/info commands/serverinfo.js
+++ commands/commands/info commands/serverinfo.js
@@ -5,22 +5,22 @@ module.exports = {
     description: 'Gets the server info!',
     guildOnly: true,
     execute(message, args) {
-        let server = message.guild;
-        let daysCreated = Math.round((message.createdTimestamp - server.createdTimestamp) / 1000 / 60 / 60 / 24)
+        let {guild} =message;
+        let daysCreated = Math.round((message.createdTimestamp - guild.createdTimestamp) / 1000 / 60 / 60 / 24)
 
         let embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
-            .setTitle(`Information on ${server.name}`)
-            .setThumbnail(server.iconURL())
+            .setTitle(`Information on ${guild.name}`)
+            .setThumbnail(guild.iconURL())
             .addFields(
-                { name: 'Servername:', value: `${server.name}`, inline: true },
-                { name: 'Owner:', value: `<@!${server.ownerID}>`, inline: true },
-                { name: 'Region:', value: `${server.region}`, inline: true },
-                { name: 'ID:', value: `${server.id}`, inline: true },
-                { name: 'Created at:', value: `\`${server.createdAt.toDateString()}\` (${daysCreated} days ago)` },
-                { name: 'Channels:', value: `${server.channels.cache.size}`, inline: true },
-                { name: 'Members:', value: `${server.memberCount}`, inline: true },
-                { name: 'Roles:', value: `${server.roles.cache.size - 1}`, inline: true },
+                { name: 'Servername:', value: `${guild.name}`, inline: true },
+                { name: 'Owner:', value: `<@!${guild.ownerID}>`, inline: true },
+                { name: 'Region:', value: `${guild.region}`, inline: true },
+                { name: 'ID:', value: `${guild.id}`, inline: true },
+                { name: 'Created at:', value: `\`${guild.createdAt.toDateString()}\` (${daysCreated} days ago)` },
+                { name: 'Channels:', value: `${guild.channels.cache.size}`, inline: true },
+                { name: 'Members:', value: `${guild.memberCount}`, inline: true },
+                { name: 'Roles:', value: `${guild.roles.cache.size - 1}`, inline: true },
             )
             .setFooter(`requested by ${message.author.tag}`)
         message.channel.send(embed);
