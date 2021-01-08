3098406739162faf58b85869507282f31d0d8d1f
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-08 21:31:13 +0000

    changed pug to html

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express()
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+app.use(express.static(`public`));
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/js', express.static(__dirname + '/js'))
+app.use('/img', express.static(__dirname + '/img'))
 
-app.use(express.static(`${__dirname}/assets`));
-app.locals.basedir = `${__dirname}/assets`;
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.get('/commands', (req, res) => res.sendFile(__dirname + '/views/commands.html'));
 
-app.get('/', (req, res) => res.render('index'));
-app.get('/commands', (req, res) => res.render('commands'));
-app.get('/login', (req, res) => res.send('This part is still under development'));
 app.get('/invite', (req, res)=>{
     console.log('button pressed')
     res.redirect('https://discord.com/api/oauth2/authorize?client_id=789960873203990598&permissions=8&scope=bot');
