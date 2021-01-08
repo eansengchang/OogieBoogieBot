36d6173b228622a348ecce81a7a7cd03457b2d18
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-08 11:23:16 +0000

    Update message.js

--- events/events/message.js
+++ events/events/message.js
@@ -27,10 +27,10 @@ module.exports = async (client, message) => {
     }
     //seif
     const seif = ['SEIF IS INSANELY SUS HOLY FUCK', 'Seif is a pedo', 'Seif? The failure of a human being?',
-        'Seif is packing a tic-tac', 'Seif has iq of room temperature', 'Seif contains much stupid', 
-        'Seif has a gay level 999', 'Seif is a big homo', 'Seif has big boobies',
-        'Seif is a stinky', 'Seif shoots up orphanages', 'Seifs tits are so massive that he can breastfeed all the starving children in Africa',
-        'Seif wants to fuck Tima', 'Seif wants to get pegged by James Charles', 'Seif ran 4Km in 15mins when he was 8 yrs old'];
+        'Seif is packing a tic-tac', 'Seif has iq of room temperature', 'Seif contains much stupid',
+        'Seif has big boobies', 'Seif is a stinky', 'Seif shoots up orphanages',
+        'Seifs tits are so massive that he can breastfeed all the starving children in Africa',
+        'Seif wants to fuck Tima', 'Seif wants to get pegged by James Charles', 'seif\'s broke the record for most thicc athelete in the world with his 4Km run in 15 mins'];
     if (content.substring(0, 4) === 'seif') {
         channel.send('<@300232634875904000> ' + seif[Math.floor(Math.random() * seif.length)]);
     }
