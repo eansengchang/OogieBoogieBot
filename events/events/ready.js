3a998df423026013532cc5cc26b2157657ae2f7f
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-11 22:16:00 +0000

    Update ready.js

--- events/events/ready.js
+++ events/events/ready.js
@@ -2,7 +2,7 @@ const config = require('@root/config.json');
 const prefix = config.prefix;
 
 module.exports = client => {
-    client.user.setActivity(`prefix: ${prefix}`, { type: 'LISTENING' });
+    client.user.setActivity(`e help`, { type: 'LISTENING' });
     client.guilds.cache.get('616347460679368731').channels.cache.get('616347460679368737').send('BOT IS ONLINE AND READY');
 
     console.log('BOT IS ONLINE AND READY');
