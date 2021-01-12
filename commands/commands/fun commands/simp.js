b7883f840e1cd1b86b39bf6054cca5cf58e019bd
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-12 14:44:35 +0000

    Create simp.js

---
+++ commands/commands/fun commands/simp.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'simp',
+    description: 'Finds out how simp you are.',
+    execute(message, args) {
+        let user = message.mentions.users.first() || message.author || message.member.user;
+        let simp = Math.round(100 * Math.random())
+
+        // if (user.id === '300232634875904000') {
+        //     simp = 1;
+        //  else if(user.id === '249148390527598592'){
+        //     simp = 101;
+        // }
+        message.channel.send(`<@${user.id}> is ${simp}% simp`);
+    },
+};
