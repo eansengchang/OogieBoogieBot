d369bc08a95517488c8148250b26fe3ac6942831
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-12 20:34:04 +0000

    Update gay.js

--- commands/commands/fun commands/gay.js
+++ commands/commands/fun commands/gay.js
@@ -5,12 +5,6 @@ module.exports = {
     execute(message, args) {
         let user = message.mentions.users.first() || message.author || message.member.user;
         let gay = Math.round(100*Math.random())
-
-        if(user.id === '300232634875904000'){
-            gay = 1;
-        } else if(user.id === '249148390527598592'){
-            gay = 101;
-        }
         message.channel.send(`<@${user.id}> is ${gay}% gay`);
     },
 };
