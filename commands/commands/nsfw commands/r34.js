90329233a249bdbc31f7dcd453d77c17e38a5887
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-12 19:40:35 +0000

    added r34

--- commands/commands/nsfw commands/r34.js
+++ commands/commands/nsfw commands/r34.js
@@ -2,18 +2,22 @@ const fetch = require('node-fetch');
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'solog',
-    description: 'Sends a solo gif!',
-    
+    name: 'r34',
+    description: 'Searches rule 34.',
+    minArgs: 1,
+    expectedArgs: '{tag}',
     async execute(message, args) {
         if (!message.channel.nsfw) return message.reply('This is not an NSFW channel');
 
-        let response = await fetch('https://nekos.life/api/v2/img/solog');
+        let response = await fetch(`https://r34-json-api.herokuapp.com/posts?tags=${args.join(' ')}`);
         let json = await response.json();
+        
+        let random = json[Math.floor(Math.random() * json.length)]
+        console.log(random)
 
         let embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
-            .setImage(json.url);
+            .setImage(random.file_url);
 
         message.channel.send(embed);
     },
