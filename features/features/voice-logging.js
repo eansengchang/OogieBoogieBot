296da3b62b2c5ccc1f5f1f4c07c5a124d04d97ec
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-16 13:08:19 +0000

    Update voice-logging.js

--- features/features/voice-logging.js
+++ features/features/voice-logging.js
@@ -1,4 +1,7 @@
 const vlogSchema = require('@models/vlog-schema');
+const { Message } = require('discord.js');
+
+let inCall = [];
 
 module.exports = async (client) => {
     //message logging
@@ -13,12 +16,38 @@ module.exports = async (client) => {
         //connects to channel
         if (state2.channel && !state1.channel) {
             vlogChannel.send(`**${state1.member.user.tag}** joined **${state2.channel.name}**`)
+            inCall.push(`${state2.member.id}-${Date.now()}`)
+            console.log(`${state2.member.user.tag} has joined in ${state2.guild.name}`)
+            console.log('incall: ', inCall)
         }
 
 
         //disconnects from a channel
         else if (state1.channel && !state2.channel) {
             let text = `**${state1.member.user.tag}** left **${state1.channel.name}** `;
+
+            let member = inCall.filter(string => {
+                return string.includes(state1.member.id)
+            })
+            inCall = inCall.filter(string => {
+                return !string.includes(state1.member.id)
+            })
+            console.log(`${state1.member.user.tag} has left in ${state1.guild.name}`)
+            console.log('member: ',member);
+            console.log('inCall: ', inCall)
+            if (!member[0]) {
+                console.log('no member[0]: ', member);
+                vlogChannel.send(text);
+                return
+            }
+
+            let array = member[0].split('-');
+            let time = Math.floor((Date.now() - array[1]) / 1000);
+
+            if (time < 60) text += `[Call time: **${time}s**]`;
+            else if (time / 60 < 60) text += (`[Call time: **${Math.floor(time / 60)}m**]`);
+            else text += (`[Call time: **${Math.floor(time / 60 / 60)}hr${Math.floor((time / 60) % 60)}m**]`);
+
             vlogChannel.send(text)
         }
 
