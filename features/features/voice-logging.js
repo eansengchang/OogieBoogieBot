829357469c83c74c5c824032bcea80f3d761e579
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-16 00:42:56 +0000

    Update voice-logging.js

--- features/features/voice-logging.js
+++ features/features/voice-logging.js
@@ -1,8 +1,4 @@
 const vlogSchema = require('@models/vlog-schema');
-const { Message } = require('discord.js');
-const fetch = require('node-fetch');
-
-let inCall = [];
 
 module.exports = async (client) => {
     //message logging
@@ -17,34 +13,12 @@ module.exports = async (client) => {
         //connects to channel
         if (state2.channel && !state1.channel) {
             vlogChannel.send(`**${state1.member.user.tag}** joined **${state2.channel.name}**`)
-            inCall.push(`${state1.member.id}-${Date.now()}`)
         }
 
 
         //disconnects from a channel
         else if (state1.channel && !state2.channel) {
             let text = `**${state1.member.user.tag}** left **${state1.channel.name}** `;
-
-            let member = inCall.filter(string => {
-                return string.includes(state1.member.id)
-            })
-            inCall = inCall.filter(string => {
-                return !string.includes(state1.member.id)
-            })
-            console.log('member: ',member);
-            console.log('incall: ', inCall)
-            if (!member[0]) {
-                console.log('no member[0]: ', member)
-                return
-            }
-
-            let array = member[0].split('-');
-            let time = Math.floor((Date.now() - array[1]) / 1000);
-
-            if (time < 60) text += `[Call time: **${time}s**]`;
-            else if (time / 60 < 60) text += (`[Call time: **${Math.floor(time / 60)}m**]`);
-            else text += (`[Call time: **${Math.floor(time / 60 / 60)}hr${(time / 60) % 60}m**]`);
-
             vlogChannel.send(text)
         }
 
