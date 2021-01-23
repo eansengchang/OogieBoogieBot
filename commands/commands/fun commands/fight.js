7807a2faa5174c862a6203b9a4aaffc12b5e8956
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-23 22:10:53 +0000

    Update fight.js

--- commands/commands/fun commands/fight.js
+++ commands/commands/fun commands/fight.js
@@ -14,6 +14,8 @@ let makeDeathMessages = (name1, name2) => {
         `${name2} *got lost and died*`,
         `${name2} *got betrayed by* ${name1} *and died*`,
         `${name2} *was slain by* ${name1}`,
+        `${name2} *accidentally shot themselves*`,
+        `${name2} *slipped on a banana and died*`,
     ]
     return deathMessage[Math.floor(Math.random() * deathMessage.length)];
 }
