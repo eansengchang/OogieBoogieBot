94be198980204e4b3d6f8ed4a9dddf5bc71dee33
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-23 21:53:47 +0000

    Update trash.js

--- commands/commands/fun commands/trash.js
+++ commands/commands/fun commands/trash.js
@@ -9,6 +9,10 @@ module.exports = {
     async execute(message, args) {
         let user = message.mentions.users.first() || message.author || message.member.user;
 
+        if(user.id === message.client.user.id){
+            return message.reply('I AM NOT TRASH')
+        }
+
         const canvas = Canvas.createCanvas(500, 500);
         const ctx = canvas.getContext('2d');
 
