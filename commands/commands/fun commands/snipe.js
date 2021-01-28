41d60338bec696013ac6208a669f46dc38c118c8
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-28 21:43:09 +0000

    tweaks

--- commands/commands/fun commands/snipe.js
+++ commands/commands/fun commands/snipe.js
@@ -10,7 +10,7 @@ module.exports = {
         const snipes = message.client.snipes.get(message.channel.id) || [];
         const msg = snipes[args[0] - 1 || 0];
         if(snipes.length === 0) return message.reply('There is nothing to snipe!')
-        if (!msg) return message.reply('That is not a valid snipe...');
+        if (!msg) return message.reply('Invalid snipe');
         const embed = new Discord.MessageEmbed()
             .setAuthor(msg.author.tag, msg.author.displayAvatarURL({ dynamic: true, size: 256 }))
             .setDescription(msg.content)
