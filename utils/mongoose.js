7cba75d4043ccfbea479f11efce4a73593e79d52
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-01-29 19:40:41 +0000

    changed env to entire mongo url

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -13,7 +13,7 @@ module.exports = {
             family:4
         };
 
-        mongoose.connect(`mongodb+srv://esc:${process.env.MONGO_PASSWORD}@activity.0d7q6.mongodb.net/Activity?retryWrites=true&w=majority`, dbOptions);
+        mongoose.connect(`${process.env.MONGO_URL}`, dbOptions);
         mongoose.set('useFindAndModify', false);
         mongoose.Promise = global.Promise;
 
