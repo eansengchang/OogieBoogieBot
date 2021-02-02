1b99e208a1f6d0dac62323b28a11ae892b829f9a
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-02-02 13:20:46 +0000

    Update play.js

--- commands/commands/sound commands/play.js
+++ commands/commands/sound commands/play.js
@@ -9,32 +9,33 @@ module.exports = {
     async execute(message, args) {
         if (message.member.voice.channel) {
 
-            let member = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
+            const member = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
             if (member) {
                 if (!fs.existsSync(`./recorded-${member.id}.pcm`)) return message.reply('no recording of user')
 
                 const connection = await message.member.voice.channel.join();
-                if(!connection) return message.channel.send('I can\'t seem to join the channel');
-
+                if (!connection) return message.channel.send('I can\'t seem to join the channel');
+                
                 const stream = fs.createReadStream(`./recorded-${member.id}.pcm`);
 
                 const dispatcher = connection.play(stream, {
-                    type: 'converted'
+                    type: 'converted',
+                    volume: 1
                 })
             }
-
+            
             else if (args[0].includes('youtu')) {
-                let connection = await message.member.voice.channel.join()
+                const connection = await message.member.voice.channel.join()
 
                 connection.play(ytdl(args[0], { filter: 'audioonly' }));
 
             } else if (args[0].endsWith('mp4') || args[0].endsWith('mp3') || args[0].endsWith('mov')) {
-                let connection = await message.member.voice.channel.join()
+                const connection = await message.member.voice.channel.join()
                 if (!connection) return message.channel.send('I can\'t seem to join the channel');
 
                 connection.play(args[0]);
             } else {
-                message.reply('That doesn\'t seem to be a valid link')
+                message.reply('That doesn\'t seem to be a valid input')
             }
 
         } else {
