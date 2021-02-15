5c709b7d5ee2f500e74c5b061d30e8591b27364d
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-02-15 11:09:27 +0000

    tweaks

--- commands/commands/voice commands/disconnect.js
+++ commands/commands/voice commands/disconnect.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'disconnect',
+    aliases: ['voicekick', 'vkick'],
     description: 'Disconnects people in a call.',
     expectedArgs: '@user / all',
     guildOnly: true,
