2ad0437b967edee0aa906cb169efcd5cefbbd1f0
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-03-01 20:12:44 +0000

    Update calc.js

--- commands/commands/info commands/calc.js
+++ commands/commands/info commands/calc.js
@@ -11,10 +11,11 @@ module.exports = {
         let resp;
         try {
             resp = math.evaluate(args.join(' '));
+            if (resp.includes(/[^0-9]/g)) throw 'Only numbers';
             await message.channel.send(resp);
         } catch {
             return message.reply('Invalid Calculation.');
         }
-        
+
     },
 };
