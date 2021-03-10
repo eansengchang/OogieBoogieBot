78f0002c85e51d9764aa16c6d0654e91dca6e25b
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-03-10 22:25:07 +0000

    Update message.js

--- events/events/message.js
+++ events/events/message.js
@@ -22,7 +22,7 @@ module.exports = async (client, message) => {
         'Pritten patil picked the wrong baby when adopting', 'Rey has big boobies',
         'Rey is a stinky', 'Rey is a curry muncher', 'Rey shoots up orphanages', 'Rey watched 300 naruto episodes in a week',
         'Rey is a weeb', 'Rey wants to fuck Tima', 'Rey got his league account banned', 'Ben chud', 'Rey wants to get pegged by Joe',
-        'Rey is hardstuck silver', 'Rey you eat cow'];
+        'Rey is hardstuck plat', 'Rey you eat cow'];
     if (content.substring(0, 3) === 'rey' && message.guild.id === '512578878305337354') {
         channel.send('<@512375511205543936> ' + rey[Math.floor(Math.random() * rey.length)]);
     }
