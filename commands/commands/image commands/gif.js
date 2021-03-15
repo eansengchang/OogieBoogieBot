0492ea20b81e7b75cc26bdc01baf18fb9dce6acf
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-03-15 22:05:01 +0000

    Create gif.js

---
+++ commands/commands/image commands/gif.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'gif',
+    examples:['cute cat'],
+    description: 'Shows a gif',
+    expectedArgs: '{tag}',
+    minArgs: 1,
+    clientPermissions: ['ATTACH_FILES'],
+    async execute(message, args) {
+        let url = `https://g.tenor.com/v1/search?q=${args.join(' ')}&key=${process.env.TENOR_KEY}&contentfilter=low`;
+        let response = await fetch(url);
+        let json = await response.json();
+        let results = json.results;
+        let random = results[Math.floor(Math.random() * results.length)];
+        message.channel.send(random.url)
+    },
+};
