8f8312722be467838175df8f3b79092739afa0c7
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-03-19 20:25:31 +0000

    Update vote.js

--- commands/commands/info commands/vote.js
+++ commands/commands/info commands/vote.js
@@ -11,24 +11,25 @@ module.exports = {
         );
         let topGGJson = await topGGFetched.json();
 
-        let BFDFetched = await fetch(`https://botsfordiscord.com/api/bot/789960873203990598/votes`,
-            {
-                headers: {
-                    "Authorization": process.env.BFD_TOKEN,
-                    "Content-Type": 'application/json'
-                }
-            }
-        );
+        // let BFDFetched = await fetch(`https://botsfordiscord.com/api/bot/789960873203990598/votes`,
+        //     {
+        //         headers: {
+        //             "Authorization": process.env.BFD_TOKEN,
+        //             "Content-Type": 'application/json'
+        //         }
+        //     }
+        // );
 
-        let BFDJson = await BFDFetched.json();
+        // let BFDJson = await BFDFetched.json();
 
         let embed = new Discord.MessageEmbed().setTitle('Vote for this bot')
             .setDescription(`[Top.gg](https://top.gg/bot/789960873203990598/vote)
             Can vote? ${topGGJson.voted === 1 ? '**False**' : '**True**'}
-            
-            [botsfordiscord.com](https://botsfordiscord.com/bot/789960873203990598/vote)
-            Can vote?: ${BFDJson.hasVoted24.includes(message.author.id) ? '**False**' : '**True**'}
             `
+            // `
+            // [botsfordiscord.com](https://botsfordiscord.com/bot/789960873203990598/vote)
+            // Can vote?: ${BFDJson.hasVoted24.includes(message.author.id) ? '**False**' : '**True**'}
+            // `
             )
 
         
