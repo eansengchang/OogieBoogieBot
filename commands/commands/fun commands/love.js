60bf36ccd64342650386c5353f84f9fea35f7c5a
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-03-28 02:22:14 +0100

    Update love.js

--- commands/commands/fun commands/love.js
+++ commands/commands/fun commands/love.js
@@ -7,6 +7,8 @@ module.exports = {
     execute(message, args) {
         let user1 = message.mentions.users.first()
         let user2 = message.mentions.users.last()
+        if(user2 === user1) user1 = message.author
+        
         let love = Math.round(Math.random() * 100);
 
         message.channel.send(`<@${user1.id}> :heart: <@${user2.id}>: ${love}%`)        
