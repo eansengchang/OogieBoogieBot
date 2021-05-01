80da4679359e7699594d2dd9b338c029de0564a3
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-05-01 14:55:47 +0100

    Delete voiceStateUpdate.js

--- events/events/voiceStateUpdate.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client, state1, state2) => {
-    let channel = state1.channel || state2.channel
-
-    membersInCall = channel.members.array()
-    if (membersInCall.length === 1 && membersInCall[0].user === client.user) {
-        state1.channel.leave();
-    }
-
-}
