fab375dfd84471d9aca89bb332a37e030b03c01b
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-05-04 09:49:10 +0100

    Update message.js

--- events/events/message.js
+++ events/events/message.js
@@ -24,7 +24,7 @@ module.exports = async (client, message) => {
         'Rey is a weeb', 'Rey wants to fuck Tima', 'Rey got his league account banned', 'Ben chud', 'Rey wants to get pegged by Joe',
         'Rey is hardstuck plat', 'Rey you eat cow'];
 
-    if ((message.guild.id === '512578878305337354' || message.guild.id === '684391250777866301') && author.id === '395152698120339456' && (content.includes('cdn.discordapp.com') || content.includes('media.discordapp.net') || message.attachments.size > 0)) {
+    if ((message.guild?.id === '512578878305337354' || message.guild?.id === '684391250777866301') && author.id === '395152698120339456' && (content.includes('cdn.discordapp.com') || content.includes('media.discordapp.net') || message.attachments.size > 0)) {
         message.reply('Not funny.')
         // await message.delete({ reason: 'arran made bad meme' }).catch(() => {})
     }
