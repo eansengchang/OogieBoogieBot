6c0d78fada55b333627e1794304f83b904d89e23
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-07-10 22:38:41 +0100

    Update calc.js

--- commands/commands/info commands/calc.js
+++ commands/commands/info commands/calc.js
@@ -8,7 +8,7 @@ module.exports = {
     minArgs: 1,
     expectedArgs: '{calculation}',
     async execute(message, args) {
-        if(author.id == "811066698790338632") return message.reply("cry")
+        if(message.author.id == "811066698790338632") return message.reply("cry")
 
         let { client } = message;
         
